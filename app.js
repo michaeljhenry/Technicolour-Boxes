977f61266b217a01ce228395082aafb1b443b833
Michael Henry <michaeljhenry@hotmail.ca>
2020-02-10 17:17:42 -0500

    technicolour boxes

---
+++ app.js
@@ -0,0 +1,48 @@
+var touchController = (function() {
+    return {
+        getColour: function() {
+            var newColour =  '#' + Math.random().toString(16).slice(2, 8); // could've chosen 3,8. We started at 2 because we wanted'
+            // to remove the "0" and "." from the decimal number. Math.random produces a number between 0 and 1 apparently. So we'd get a 0.1F4FA9EB...
+
+            return newColour;
+        }
+    }
+})();
+
+var UIController = (function() {
+    return {
+        displayColour: function(newColour) {
+            event.target.style.backgroundColor = newColour;
+            event.target.style.boxShadow = '0 0 5px #ffffff';
+        },
+
+        removeShadow: function() {
+            event.target.style.boxShadow = '';
+        }
+    }
+})();
+
+var controller = (function(touchCtrl, UICtrl) {
+    var setupEventListeners = function() {
+        document.querySelectorAll(".box").forEach(function(el) {
+        el.addEventListener("mouseover", addColourCtrl);
+        });
+        document.querySelectorAll(".box").forEach(function(el) {
+            el.addEventListener("mouseout", removeShadowCtrl);
+            });
+    }
+
+    var addColourCtrl = function() {
+        console.log("hi");
+        var colour = touchCtrl.getColour();
+        console.log("hi");
+
+        UICtrl.displayColour(colour);
+    }
+
+    var removeShadowCtrl = function () {
+        UICtrl.removeShadow();
+    }
+
+    setupEventListeners();
+})(touchController,UIController);
